865c4db1a20c552c22f253923d913c7ff62ca19d
Anthony Leedom <anthonym.leedom@gmail.com>
2025-03-27 13:50:00 -0700

    More transcode options and performance improvements.

--- include/esc/detail/transcode.hpp
+++ include/esc/detail/transcode.hpp
@@ -10,6 +10,7 @@ namespace esc::detail {
 
 /**
  * Convert the given char32_t to its UTF8 reprensentation.
+ * @details The input must be a valid UTF32 sequence, otherwise UB.
  * @param c The char32_t to convert.
  * @return The UTF8 bytes.
  */
@@ -18,11 +19,21 @@ auto u32_to_u8(char32_t c) -> std::string;
 
 /**
  * Convert the given char32_t string to its UTF8 reprensentation.
+ * @details The input must be a valid UTF32 sequence, otherwise UB.
  * @param s The char32_t string to convert.
  * @return The UTF8 bytes.
  */
 [[nodiscard]]
-auto u32_to_u8(std::u32string_view s) -> std::string;
+auto u32_to_u8(std::u32string_view sv) -> std::string;
+
+/**
+ * Convert the given char32_t string to its UTF8 reprensentation.
+ * @details The input must be a valid UTF32 sequence, otherwise UB.
+ * @param s The char32_t string to convert.
+ * @param out The string to write out the utf8 bytes to, it will be `clear()`ed.
+ */
+[[nodiscard]]
+void u32_to_u8(std::u32string_view sv, std::string& out);
 
 /**
  * Convert a UTF8 array of bytes into a UTF32 char32_t.
@@ -41,11 +52,19 @@ auto u8_to_u32(std::array<char, 4> bytes) -> char32_t;
 auto u8_string_to_u32(std::string_view sv) -> zzz::Generator<char32_t>;
 
 /**
- * Convert a UTF8 string to a UTF32 string
+ * Convert a UTF8 string to a UTF32 string.
  * @param sv The UTF8 string to convert.
  * @return A UTF32 std::u32string.
  */
 [[nodiscard]]
 auto u8_string_to_u32_string(std::string_view sv) -> std::u32string;
+
+/**
+ * Convert a UTF8 string to a UTF32 string.
+ * @param sv The UTF8 string to convert.
+ * @param out The u32string to write out the utf32 bytes to, it will be `clear()`ed.
+ */
+[[nodiscard]]
+void u8_string_to_u32_string(std::string_view sv, std::u32string& out);
 
 }  // namespace esc::detail
--- src/detail/transcode.cpp
+++ src/detail/transcode.cpp
@@ -16,17 +16,30 @@ namespace esc::detail {
 
 auto u32_to_u8(char32_t c) -> std::string
 {
+    auto buffer = std::array<char, U8_MAX_LENGTH>{};  // UTF-8 max length is 4 bytes
+    auto i = 0;
+    U8_APPEND_UNSAFE(buffer.data(), i, static_cast<UChar32>(c));  // ICU macro
+    return std::string{buffer.data(), static_cast<std::size_t>(i)};
+}
+
+auto u32_to_u8(std::u32string_view sv) -> std::string
+{
     auto result = std::string{};
-    icu::UnicodeString{static_cast<UChar32>(c)}.toUTF8String(result);
+    u32_to_u8(sv, result);
     return result;
 }
 
-auto u32_to_u8(std::u32string_view s) -> std::string
+void u32_to_u8(std::u32string_view sv, std::string& out)
 {
-    auto result = std::string{};
-    icu::UnicodeString::fromUTF32(reinterpret_cast<UChar32 const*>(s.data()), s.size())
-        .toUTF8String(result);
-    return result;
+    out.clear();
+    out.reserve(sv.size());  // Common case every char is 1 byte.
+
+    for (auto c : sv) {
+        char utf8_buffer[U8_MAX_LENGTH];
+        auto i = 0;
+        U8_APPEND_UNSAFE(utf8_buffer, i, static_cast<UChar32>(c));
+        out.append(utf8_buffer, i);
+    }
 }
 
 auto u8_to_u32(std::array<char, 4> bytes) -> char32_t
@@ -54,18 +67,24 @@ auto u8_string_to_u32(std::string_view sv) -> zzz::Generator<char32_t>
 [[nodiscard]]
 auto u8_string_to_u32_string(std::string_view sv) -> std::u32string
 {
+    auto result = std::u32string{};
+    u8_string_to_u32_string(sv, result);
+    return result;
+}
+
+void u8_string_to_u32_string(std::string_view sv, std::u32string& out)
+{
     auto i = std::int32_t{0};  // Index in the UTF-8 string
     auto const length = static_cast<std::int32_t>(sv.length());
 
-    auto result = std::u32string{};
-    result.reserve(sv.length());
+    out.clear();
+    out.reserve(sv.length());
     while (i < length) {
         UChar32 ch;
         U8_NEXT(sv.data(), i, length, ch);
         if (ch < 0) { throw std::runtime_error{"Invalid UTF-8 sequence"}; }
-        result.push_back(static_cast<char32_t>(ch));
+        out.push_back(static_cast<char32_t>(ch));
     }
-    return result;
 }
 
 }  // namespace esc::detail
